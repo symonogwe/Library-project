009b37390c27d161de95e26385e6ca4a997c70be
symonogwe <symonogwe@gmail.com>
2023-07-25 13:08:37 +0300

    Add styles to the delete buttons
    - Create delete-btn class and link it to the delete buttons using
    DOM
    - Style the buttons and add a hover effect to the buttons

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ function displayBooks() {
 
       let removeBtn = document.createElement("button");
       removeBtn.textContent = "Delete";
+      removeBtn.classList.add("delete-btn");
       removeBtn.addEventListener("click", () => {
         let removed = myLibrary.splice(divCard.dataset.index, 1);
         console.log(myLibrary);
@@ -131,6 +132,7 @@ function displayBooks() {
 
     let removeBtn = document.createElement("button");
     removeBtn.textContent = "Delete";
+    removeBtn.classList.add("delete-btn");
     removeBtn.addEventListener("click", () => {
       let removed = myLibrary.splice(divCard.dataset.index, 1);
       console.log(myLibrary);
